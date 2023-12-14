b0202a4e8a999b65d1fff7b3d5d4662d970c3cb6
ddealsan <ddealsan@myuax.com>
2023-12-14 11:19:46 +0100

    avanzamos codigo

--- main.cpp
+++ main.cpp
@@ -2,3 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define SIZE 4096
+
+int main() {
+    // Crear una vista de memoria compartida
+    HANDLE hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, SIZE, L"MySharedMemory");
+    if (hMapFile == NULL) {
+        perror("CreateFileMapping");
+        exit(EXIT_FAILURE);
+    }
+
+    // Mapear la vista de memoria compartida en el espacio de direcciones del proceso
+    LPVOID shared_memory = MapViewOfFile(hMapFile, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, SIZE);
+    if (shared_memory == NULL) {
+        perror("MapViewOfFile");
+        CloseHandle(hMapFile);
+        exit(EXIT_FAILURE);
+    }
